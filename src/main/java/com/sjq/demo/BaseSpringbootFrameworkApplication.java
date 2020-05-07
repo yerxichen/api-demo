46137582b5119f6192742801c9616a2a36aca47a
yerxichen <879908188@qq.com>
2020-05-07 19:58:30 +0800

    初始化

---
+++ src/main/java/com/sjq/demo/BaseSpringbootFrameworkApplication.java
@@ -0,0 +1,18 @@
+package com.sjq.demo;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
+@MapperScan("com.sjq.demo.mapper")
+public class BaseSpringbootFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseSpringbootFrameworkApplication.class, args);
+    }
+
+}
